b949f33b581218496ade47f9ddbbb36d9c996f19
Aneesh Raj <aneeshptr007@gmail.com>
2020-08-05 02:41:19 +1000

    Initial Commit

---
+++ modules/eks/allow_nodes.tf
@@ -0,0 +1,39 @@
+########################################################################################
+# setup provider for kubernetes
+
+#data "external" "aws_iam_authenticator" {
+#  program = ["sh", "-c", "aws-iam-authenticator token -i example | jq -r -c .status"]
+#}
+
+data "aws_eks_cluster_auth" "tf_eks" {
+  name = "${aws_eks_cluster.tf_eks.name}"
+}
+
+
+provider "kubernetes" {
+  host                      = "${aws_eks_cluster.tf_eks.endpoint}"
+  cluster_ca_certificate    = "${base64decode(aws_eks_cluster.tf_eks.certificate_authority.0.data)}"
+ #token                     = "${data.external.aws_iam_authenticator.result.token}"
+ token                      = "${data.aws_eks_cluster_auth.tf_eks.token}"
+  load_config_file          = false
+  version = "~> 1.5"
+}
+
+# Allow worker nodes to join cluster via config map
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name = "aws-auth"
+    namespace = "kube-system"
+  }
+  data = {
+    mapRoles = <<EOF
+- rolearn: ${aws_iam_role.tf-eks-node.arn}
+  username: system:node:{{EC2PrivateDNSName}}
+  groups:
+    - system:bootstrappers
+    - system:nodes
+EOF
+  }
+  depends_on = [
+    "aws_eks_cluster.tf_eks","aws_autoscaling_group.tf_eks"] 
+}
